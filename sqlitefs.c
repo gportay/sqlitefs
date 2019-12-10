4435c60f031c43e92c9e1803a87657b7ba7deae9
Gaël PORTAY <gael.portay@collabora.com>
2019-12-09 22:15:19 -0500

    Use SPDX license identifier

--- sqlitefs.c
+++ sqlitefs.c
@@ -2,21 +2,7 @@
  *  Copyright (C) 2018 Savoir-Faire Linux Inc.
  *                2018 Gaël PORTAY
  *
- *  Authors:
- *       Gaël PORTAY <gael.portay@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  SPDX-License-Identifier: LGPL-2.1
  */
 
 #define FUSE_USE_VERSION 30
@@ -35,18 +21,11 @@ const char VERSION[] = __DATE__ " " __TIME__;
 #include <getopt.h>
 #include <pwd.h>
 #include <grp.h>
-#include <sys/xattr.h>
 
 #include <fuse.h>
 #include <sqlite3.h>
 
 #include "hexdump.h"
-
-#define __max(a,b) ({ \
-	__typeof__ (a) _a = (a); \
-	__typeof__ (b) _b = (b); \
-	_a > _b ? _a : _b; \
-})
 
 #define __min(a,b) ({ \
 	__typeof__ (a) _a = (a); \
@@ -295,13 +274,8 @@ static int add_directory(sqlite3 *db, const char *file, const char *parent,
 /** Get file attributes.
  *
  * Similar to stat().  The 'st_dev' and 'st_blksize' fields are
- * ignored. The 'st_ino' field is ignored except if the 'use_ino'
- * mount option is given. In that case it is passed to userspace,
- * but libfuse and the kernel will still assign a different
- * inode for internal use (called the "nodeid").
- *
- * `fi` will always be NULL if the file is not currently open, but
- * may also be NULL if the file is open.
+ * ignored.	 The 'st_ino' field is ignored except if the 'use_ino'
+ * mount option is given.
  */
 static int sqlitefs_getattr(const char *path, struct stat *st)
 {
@@ -316,7 +290,8 @@ static int sqlitefs_getattr(const char *path, struct stat *st)
 
 	if (!db) {
 		fprintf(stderr, "%s: Invalid context\n", __FUNCTION__);
-		return -EINVAL;
+		errno = EINVAL;
+		return -1;
 	}
 
 	snprintf(sql, sizeof(sql), "SELECT "
@@ -342,187 +317,16 @@ static int sqlitefs_getattr(const char *path, struct stat *st)
 	if (ret != SQLITE_OK) {
 		fprintf(stderr, "sqlite3_exec: %s\n", e);
 		sqlite3_free(e);
-		return -EACCES;
-	}
-
-	if (data.error) {
-		return -data.error;
-	}
-
-	fprintstat(stderr, path, st);
-
-	return 0;
-}
-
-/** Read the target of a symbolic link
- *
- * The buffer should be filled with a null terminated string.  The
- * buffer size argument includes the space for the terminating
- * null character.	If the linkname is too long to fit in the
- * buffer, it should be truncated.	The return value should be 0
- * for success.
- */
-static int squasfs_readlink(const char *path, char *buf, size_t bufsize)
-{
-	sqlite3 *db = fuse_get_context()->private_data;
-	(void)path;
-	(void)buf;
-	(void)bufsize;
-
-	if (!db) {
-		fprintf(stderr, "%s: Invalid context\n", __FUNCTION__);
-		errno = EINVAL;
+		errno = EACCES;
 		return -1;
 	}
 
-	return -1;
-}
-
-/* Deprecated, use readdir() instead */
-/* int (*getdir) (const char *, fuse_dirh_t, fuse_dirfil_t); */
-
-/** Create a file node
- *
- * This is called for creation of all non-directory, non-symlink
- * nodes.  If the filesystem defines a create() method, then for
- * regular files that will be called instead.
- */
-int sqlitefs_mknod(const char *path, mode_t mode, dev_t dev)
-{
-	sqlite3 *db = fuse_get_context()->private_data;
-	struct stat st;
-
-	if (!db) {
-		fprintf(stderr, "%s: Invalid context\n", __FUNCTION__);
-		return -EINVAL;
+	if (data.error) {
+		errno = data.error;
+		return -1;
 	}
 
-	fprintf(stderr, "%s(path: %s, mode: %x, dev: %li)\n", __FUNCTION__,
-		path, mode, dev);
-
-	memset(&st, 0, sizeof(struct stat));
-	/* Ignored st.st_dev = 0; */
-	/* Ignored st.st_ino = 0; */
-	st.st_mode = S_IFREG | 0644;
-	st.st_nlink = 1;
-	st.st_uid = getuid();
-	st.st_gid = getgid();
-	/* Ignored st.st_blksize = 0; */
-	st.st_atime = time(NULL);
-	st.st_mtime = time(NULL);
-	st.st_ctime = time(NULL);
-	st.st_size = 0;
-	if (add_file(db, path, "/", "", 0, &st)) {
-		sqlite3_close(db);
-		return -EIO;
-	}
-
-	return 0;
-}
-
-/** Change the permission bits of a file
- *
- * `fi` will always be NULL if the file is not currenlty open, but
- * may also be NULL if the file is open.
- */
-#if 0
-static int sqlitefs_chmod(const char *path, mode_t mode,
-			  struct fuse_file_info *fi)
-{
-	fprintf(stderr, "%s(path: %s, mode: %x, fi: %p)\n", __FUNCTION__, path,
-		mode, fi);
-
-	return 0;
-}
-#else
-static int sqlitefs_chmod(const char *path, mode_t mode)
-{
-	fprintf(stderr, "%s(path: %s, mode: %x)\n", __FUNCTION__, path, mode);
-
-	return 0;
-}
-#endif
-
-/** Change the owner and group of a file
- *
- * `fi` will always be NULL if the file is not currenlty open, but
- * may also be NULL if the file is open.
- *
- * Unless FUSE_CAP_HANDLE_KILLPRIV is disabled, this method is
- * expected to reset the setuid and setgid bits.
- */
-#if 0
-static int sqlitefs_chown(const char *path, uid_t uid, gid_t gid,
-			  struct fuse_file_info *fi)
-{
-	fprintf(stderr, "%s(path: %s, uid: %i, gid: %i, fi: %p)\n",
-		__FUNCTION__, path, uid, gid, fi);
-
-	return 0;
-}
-#else
-static int sqlitefs_chown(const char *path, uid_t uid, gid_t gid)
-{
-	fprintf(stderr, "%s(path: %s, uid: %i, gid: %i)\n", __FUNCTION__, path,
-		uid, gid);
-
-	return 0;
-}
-#endif
-
-/** Change the size of a file
- *
- * `fi` will always be NULL if the file is not currenlty open, but
- * may also be NULL if the file is open.
- *
- * Unless FUSE_CAP_HANDLE_KILLPRIV is disabled, this method is
- * expected to reset the setuid and setgid bits.
- */
-#if 0
-static int sqlitefs_truncate(const char *path, off_t size,
-			     struct fuse_file_info *fi)
-{
-	fprintf(stderr, "%s(path: %s, size: %li, fi: %p)\n", __FUNCTION__, path,
-		size, fi);
-
-	return 0;
-}
-#else
-static int sqlitefs_truncate(const char *path, off_t size)
-{
-	fprintf(stderr, "%s(path: %s, size: %li)\n", __FUNCTION__, path, size);
-
-	return 0;
-}
-#endif
-
-/** File open operation
- *
- * No creation (O_CREAT, O_EXCL) and by default also no
- * truncation (O_TRUNC) flags will be passed to open(). If an
- * application specifies O_TRUNC, fuse first calls truncate()
- * and then open(). Only if 'atomic_o_trunc' has been
- * specified and kernel version is 2.6.24 or later, O_TRUNC is
- * passed on to open.
- *
- * Unless the 'default_permissions' mount option is given,
- * open should check if the operation is permitted for the
- * given flags. Optionally open may also return an arbitrary
- * filehandle in the fuse_file_info structure, which will be
- * passed to all file operations.
- */
-static int sqlitefs_open(const char *path, struct fuse_file_info *fuse)
-{
-	sqlite3 *db = fuse_get_context()->private_data;
-	(void)path;
-	(void)fuse;
-
-	if (!db) {
-		fprintf(stderr, "%s: Invalid context\n", __FUNCTION__);
-		return -EINVAL;
-	}
-
-	fprintf(stderr, "%s(path: %s, fuse: %p)\n", __FUNCTION__, path, fuse);
+	fprintstat(stderr, path, st);
 
 	return 0;
 }
@@ -535,6 +339,8 @@ static int sqlitefs_open(const char *path, struct fuse_file_info *fuse)
  * 'direct_io' mount option is specified, in which case the return
  * value of the read system call will reflect the return value of
  * this operation.
+ *
+ * Changed in version 2.2
  */
 int sqlitefs_read(const char *path, char *buf, size_t bufsize, off_t offset,
 	     struct fuse_file_info *fi)
@@ -547,7 +353,8 @@ int sqlitefs_read(const char *path, char *buf, size_t bufsize, off_t offset,
 
 	if (!db) {
 		fprintf(stderr, "%s: Invalid context\n", __FUNCTION__);
-		return -EINVAL;
+		errno = EINVAL;
+		return -1;
 	}
 
 	snprintf(sql, sizeof(sql),
@@ -594,76 +401,26 @@ exit:
 	return size;
 }
 
-/** Release an open file
- *
- * Release is called when there are no more references to an open
- * file: all file descriptors are closed and all memory mappings
- * are unmapped.
- *
- * For every open() call there will be exactly one release() call
- * with the same flags and file descriptor.	 It is possible to
- * have a file opened more than once, in which case only the last
- * release will mean, that no more reads/writes will happen on the
- * file.  The return value of release is ignored.
- */
-static int sqlitefs_release(const char *path, struct fuse_file_info *fi)
-{
-	fprintf(stderr, "%s(path: %s, fi: %p)\n", __FUNCTION__, path, fi);
-
-	return 0;
-}
-
-/** Set extended attributes */
-static int sqlitefs_setxattr(const char *path, const char *name,
-			     const char *value, size_t size, int flags)
-{
-	fprintf(stderr, "%s(path: %s, name: %s, value: %s, size: %li, flags: %i)\n",
-		__FUNCTION__, path, name, value, size, flags);
-
-	return 0;
-}
-
-/** Get extended attributes */
-static int sqlitefs_getxattr(const char *path, const char *name, char *value,
-			     size_t size)
-{
-	fprintf(stderr, "%s(path: %s, name: %s, value: %s, size: %li)\n",
-		__FUNCTION__, path, name, value, size);
-
-	return 0;
-}
-
-/** List extended attributes */
-static int sqlitefs_listxattr(const char *path, char *list, size_t size)
-{
-	fprintf(stderr, "%s(path: %s, list: %s, size: %li)\n", __FUNCTION__,
-		path, list, size);
-
-	return 0;
-}
-
-/** Remove extended attributes */
-static int sqlitefs_removexattr(const char *path, const char *name)
-{
-	fprintf(stderr, "%s(path: %s, name: %s)\n", __FUNCTION__, path, name);
-
-	return 0;
-}
-
 /** Read directory
+ *
+ * This supersedes the old getdir() interface.  New applications
+ * should use this.
  *
  * The filesystem may choose between two modes of operation:
  *
  * 1) The readdir implementation ignores the offset parameter, and
  * passes zero to the filler function's offset.  The filler
  * function will not return '1' (unless an error happens), so the
- * whole directory is read in a single readdir operation.
+ * whole directory is read in a single readdir operation.  This
+ * works just like the old getdir() method.
  *
  * 2) The readdir implementation keeps track of the offsets of the
  * directory entries.  It uses the offset parameter and always
  * passes non-zero offset to the filler function.  When the buffer
  * is full (or an error happens) the filler function will return
  * '1'.
+ *
+ * Introduced in version 2.3
  */
 static int sqlitefs_readdir(const char *path, void *buffer,
 			    fuse_fill_dir_t filler, off_t offset,
@@ -683,7 +440,8 @@ static int sqlitefs_readdir(const char *path, void *buffer,
 
 	if (!db) {
 		fprintf(stderr, "%s: Invalid context\n", __FUNCTION__);
-		return -EINVAL;
+		errno = EINVAL;
+		return -1;
 	}
 
 	filler(buffer, ".", NULL, 0);
@@ -695,102 +453,11 @@ static int sqlitefs_readdir(const char *path, void *buffer,
 	if (ret != SQLITE_OK) {
 		fprintf(stderr, "sqlite3_exec: %s\n", e);
 		sqlite3_free(e);
-		return -EACCES;
+		errno = EACCES;
+		return -1;
 	}
 
 	return 0;
-}
-
-/**
- * Create and open a file
- *
- * If the file does not exist, first create it with the specified
- * mode, and then open it.
- *
- * If this method is not implemented or under Linux kernel
- * versions earlier than 2.6.15, the mknod() and open() methods
- * will be called instead.
- */
-static int sqlitefs_create(const char *path, mode_t mode,
-			   struct fuse_file_info *f)
-{
-	(void)path;
-	(void)mode;
-	(void)f;
-	sqlite3 *db = fuse_get_context()->private_data;
-
-	if (!db) {
-		fprintf(stderr, "%s: Invalid context\n", __FUNCTION__);
-		errno = EINVAL;
-		return -1;
-	}
-
-	return -1;
-}
-
-/** Create a directory
- *
- * Note that the mode argument may not have the type specification
- * bits set, i.e. S_ISDIR(mode) can be false.  To obtain the
- * correct directory type bits use  mode|S_IFDIR
- * */
-static int sqlitefs_mkdir(const char *path, mode_t mode)
-{
-	sqlite3 *db = fuse_get_context()->private_data;
-	(void)path;
-	(void)mode;
-
-	if (!db) {
-		fprintf(stderr, "%s: Invalid context\n", __FUNCTION__);
-		errno = EINVAL;
-		return -1;
-	}
-
-	return -1;
-}
-
-/** Remove a file */
-static int sqlitefs_unlink(const char *path)
-{
-	sqlite3 *db = fuse_get_context()->private_data;
-	(void)path;
-
-	if (!db) {
-		fprintf(stderr, "%s: Invalid context\n", __FUNCTION__);
-		errno = EINVAL;
-		return -1;
-	}
-
-	return -1;
-}
-
-/** Remove a directory */
-static int sqlitefs_rmdir(const char *path)
-{
-	sqlite3 *db = fuse_get_context()->private_data;
-	(void)path;
-
-	if (!db) {
-		errno = EINVAL;
-		return -1;
-	}
-
-	return -1;
-}
-
-/** Create a symbolic link */
-static int sqlitefs_symlink(const char *path1, const char *path2)
-{
-	sqlite3 *db = fuse_get_context()->private_data;
-	(void)path1;
-	(void)path2;
-
-	if (!db) {
-		fprintf(stderr, "%s: Invalid context\n", __FUNCTION__);
-		return -EINVAL;
-	}
-
-	return -EIO;
 }
 
 /**
@@ -905,20 +572,8 @@ static void sqlitefs_destroy(void *ptr)
 
 static struct fuse_operations operations = {
 	.getattr = sqlitefs_getattr,
-	.mknod = sqlitefs_mknod,
-//	.unlink = sqlitefs_unlink,
-//	.rmdir = sqlitefs_rmdir,
-//	.symlink = sqlitefs_symlink,
-	.open = sqlitefs_open,
 	.read = sqlitefs_read,
-	.release = sqlitefs_release,
-	.chmod = sqlitefs_chmod,
-	.chown = sqlitefs_chown,
-	.truncate = sqlitefs_truncate,
-//	.utimens = sqlitefs_utimens,
-	.setxattr = sqlitefs_setxattr,
 	.readdir = sqlitefs_readdir,
-//	.create = sqlitefs_create,
 	.init = sqlitefs_init,
 	.destroy = sqlitefs_destroy,
 };
