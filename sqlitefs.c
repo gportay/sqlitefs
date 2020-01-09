gportay/sqlitefs

commit 279a3bf4e0e7ac7c54073872c177bad6f8a3e6b5

Intercept debug option
This commit intercepts the debug option from the command-line and prints
the debug traces only if the debug option is set twice:

	$ sqlitefs -d -d mountpoint/

The option parsing is inspired from[1].

[1]: https://github.com/libfuse/libfuse/wiki/Option-Parsing

sqlitefs.c
@@ -28,6 +28,12 @@ const char VERSION[] = __DATE__ " " __TIME__;
 
 #include "hexdump.h"
 
+static int DEBUG = 0;
+#define debug(fmt, ...) if (DEBUG) fprintf(stderr, fmt, ##__VA_ARGS__)
+#define debug2(fmt, ...) if (DEBUG >= 2) fprintf(stderr, fmt, ##__VA_ARGS__)
+#define hexdebug(addr, buf, size) if (DEBUG) fhexdump(stderr, addr, buf, size)
+#define hexdebug2(addr, buf, size) if (DEBUG >= 2) fhexdump(stderr, addr, buf, size)
+
 #define __min(a,b) ({ \
 	__typeof__ (a) _a = (a); \
 	__typeof__ (b) _b = (b); \
@@ -410,7 +416,8 @@ static int __stat(sqlite3 *db, const char *path, struct stat *st)
 	if (data.error)
 		return -data.error;
 
-	fprintstat(stderr, path, st);
+	if (DEBUG >= 2)
+		fprintstat(stderr, path, st);
 
 	return 0;
 }
@@ -541,7 +548,7 @@ exit:
 	if (ret)
 		return ret;
 
-	fhexdump(stderr, offset, buf, size);
+	hexdebug2(offset, buf, size);
 
 	return size;
 }
@@ -604,7 +611,7 @@ exit:
 	if (ret)
 		return ret;
 
-	fhexdump(stderr, offset, buf, bufsize);
+	hexdebug2(offset, buf, bufsize);
 
 	return bufsize;
 }
@@ -783,7 +790,8 @@ static int __readlink(sqlite3 *db, const char *path, char *buf, size_t len)
 	if (data.error)
 		return -data.error;
 
-	fprintf(stderr, "%s\n", buf);
+	if (DEBUG)
+		fprintf(stderr, "%s\n", buf);
 
 	return 0;
 }
@@ -1740,8 +1748,50 @@ static struct fuse_operations operations = {
 	/* .copy_file_range */
 };
 
+enum {
+	KEY_DEBUG,
+	KEY_HELP,
+	KEY_VERSION,
+};
+
+static struct fuse_opt sqlitefs_opts[] = {
+	FUSE_OPT_KEY("-d",              KEY_DEBUG),
+	FUSE_OPT_KEY("debug",           KEY_DEBUG),
+	FUSE_OPT_KEY("-V",		KEY_VERSION),
+	FUSE_OPT_KEY("--version",	KEY_VERSION),
+	FUSE_OPT_KEY("-h",		KEY_HELP),
+	FUSE_OPT_KEY("--help",		KEY_HELP),
+	FUSE_OPT_END
+};
+
+static int sqlitefs_opt_proc(void *data, const char *arg, int key,
+			     struct fuse_args *outargs)
+{
+	(void)data;
+
+	switch (key) {
+	case KEY_DEBUG:
+		DEBUG++;
+		break;
+
+	case KEY_HELP:
+		fuse_opt_add_arg(outargs, arg);
+		fuse_main(outargs->argc, outargs->argv, &operations, NULL);
+		exit(EXIT_SUCCESS);
+
+	case KEY_VERSION:
+		fprintf(stderr, "SQLiteFS version %s\n", VERSION);
+		fuse_opt_add_arg(outargs, arg);
+		fuse_main(outargs->argc, outargs->argv, &operations, NULL);
+		exit(EXIT_SUCCESS);
+	}
+
+	return 1;
+}
+
 int main(int argc, char *argv[])
 {
+	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
 	sqlite3 *db;
 
 	if (__strncmp(basename(argv[0]), "mkfs.sqlitefs") == 0) {
@@ -1750,6 +1800,12 @@ int main(int argc, char *argv[])
 			__exit_perror("fs.db", -err);
 
 		return EXIT_SUCCESS;
+	}
+
+	if (fuse_opt_parse(&args, NULL, sqlitefs_opts, sqlitefs_opt_proc)) {
+		char * args[] = { argv[0], "--help" };
+		fuse_main(sizeof(args)/sizeof(char *), args, &operations, NULL);
+		__exit_perror("fuse_opt_parse", EINVAL);
 	}
 
 	if (sqlite3_open_v2("fs.db", &db, SQLITE_OPEN_READWRITE, NULL)) {

commit 25e8ae833059fc367171011a6029548f1beb5133

Fix memory leak
A memory leak was introduced with commit 279a3bf (Intercept debug
option).

It can be reproduced using the command below:

	$ EXEC="sleep 1" valgrind -v --leak-check=full sqlitefs -f mountpoint/
	==3254877==
	==3254877== HEAP SUMMARY:
	==3254877==     in use at exit: 58 bytes in 4 blocks
	==3254877==   total heap usage: 224 allocs, 220 frees, 2,379,849 bytes allocated
	==3254877==
	==3254877== Searching for pointers to 4 not-freed blocks
	==3254877== Checked 132,168 bytes
	==3254877==
	==3254877== 58 (32 direct, 26 indirect) bytes in 1 blocks are definitely lost in loss record 4 of 4
	==3254877==    at 0x483AD7B: realloc (vg_replace_malloc.c:836)
	==3254877==    by 0x49C73CC: fuse_opt_add_arg (in /usr/lib/libfuse3.so.3.9.0)
	==3254877==    by 0x49C7B66: fuse_opt_parse (in /usr/lib/libfuse3.so.3.9.0)
	==3254877==    by 0x10E3A0: main (sqlitefs.c:1880)
	==3254877==
	==3254877== LEAK SUMMARY:
	==3254877==    definitely lost: 32 bytes in 1 blocks
	==3254877==    indirectly lost: 26 bytes in 3 blocks
	==3254877==      possibly lost: 0 bytes in 0 blocks
	==3254877==    still reachable: 0 bytes in 0 blocks
	==3254877==         suppressed: 0 bytes in 0 blocks
	==3254877==
	==3254877== ERROR SUMMARY: 1 errors from 1 contexts (suppressed: 0 from 0)

sqlitefs.c
@@ -1882,6 +1882,7 @@ int main(int argc, char *argv[])
 		usage(argv[0]);
 		__exit_perror("fuse_opt_parse", EINVAL);
 	}
+	fuse_opt_free_args(&args);
 
 	if (getenv("EXEC")) {
 		if (!foreground && !DEBUG) {